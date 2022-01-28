9caa3d5c206805e54935df346552444da123a49e
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-01-28 15:23:52 +0300

    Update cells

--- CollectionViewXIB/MainModule/MainViewController.swift
+++ CollectionViewXIB/MainModule/MainViewController.swift
@@ -14,27 +14,31 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        initializeCollectionView()
+        
+    }
+    
+    func initializeCollectionView() {
         let layout = UICollectionViewFlowLayout()
         collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height), collectionViewLayout: layout)
-        layout.itemSize = CGSize(width: 300, height: 200)
+        layout.itemSize = CGSize(width: self.view.frame.width - 40, height: 200)
         layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         layout.scrollDirection = .vertical
-        self.view.addSubview(collectionView)
-        
         collectionView.backgroundColor = .gray
+
+        self.view.addSubview(collectionView)
         
         collectionView.dataSource = self
         collectionView.delegate = self
         
         let nib = UINib(nibName: CollectionViewCell.reuseId, bundle: nil)
         collectionView.register(nib, forCellWithReuseIdentifier: CollectionViewCell.reuseId)
-        
     }
 }
 
 extension MainViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 3
+        return 7
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -42,6 +46,8 @@ extension MainViewController: UICollectionViewDataSource, UICollectionViewDelega
         cell.titleLabel.text = "Title"
         cell.descriptionLabel.text = "Lorem Ipsam"
         cell.backgroundColor = .green
+        cell.layer.cornerRadius = 20
+        cell.layer.masksToBounds = true
         return cell
     }
 }
