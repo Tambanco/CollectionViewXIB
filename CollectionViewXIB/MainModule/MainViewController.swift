5b7722d38c25d1f60eb636defffbddf32bdf496d
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-01-28 15:15:36 +0300

    Update CollectionView

--- CollectionViewXIB/MainModule/MainViewController.swift
+++ CollectionViewXIB/MainModule/MainViewController.swift
@@ -15,14 +15,19 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         
         let layout = UICollectionViewFlowLayout()
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height), collectionViewLayout: layout)
+        layout.itemSize = CGSize(width: 300, height: 200)
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         layout.scrollDirection = .vertical
         self.view.addSubview(collectionView)
+        
+        collectionView.backgroundColor = .gray
         
         collectionView.dataSource = self
         collectionView.delegate = self
         
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: CollectionViewCell.reuseId)
+        let nib = UINib(nibName: CollectionViewCell.reuseId, bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: CollectionViewCell.reuseId)
         
     }
 }
@@ -33,8 +38,10 @@ extension MainViewController: UICollectionViewDataSource, UICollectionViewDelega
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
-        cell.backgroundColor = .gray
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        cell.titleLabel.text = "Title"
+        cell.descriptionLabel.text = "Lorem Ipsam"
+        cell.backgroundColor = .green
         return cell
     }
 }
