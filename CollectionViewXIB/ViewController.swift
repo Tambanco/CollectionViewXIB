f179e2cc751ec2147c75913eccd9ac506c40356b
Tambanco <52546246+Tambanco@users.noreply.github.com>
2022-01-28 13:52:46 +0300

    Add project files

---
+++ CollectionViewXIB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewXIB
+//
+//  Created by tambanco 🥳 on 28.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
